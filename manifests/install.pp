81dc71bec8112293d8e69d101161bdb03934a069
Kyrill Poole <kyrill@pancentric.com>
2016-07-22 10:41:01 +0100

    Set package name so fluentd gets installed

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@
 #
 class fluentd::install inherits fluentd {
 
-  package { 'fluentd':
+  package { "$::fluentd::package_name":
     ensure          => $::fluentd::package_ensure,
     name            => $::fluentd::package_name,
     install_options => $::fluentd::package_install_options,
