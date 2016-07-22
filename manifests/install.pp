0afc13d29b6ad5bc78b49c83020d6fdfc53266fb
Kyrill Poole <kyrill@pancentric.com>
2016-07-22 10:46:03 +0100

    Added catchall

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,10 +2,15 @@
 #
 class fluentd::install inherits fluentd {
 
-  package { "$::fluentd::package_name":
+  package { '$::fluentd::package_name':
     ensure          => $::fluentd::package_ensure,
     name            => $::fluentd::package_name,
     install_options => $::fluentd::package_install_options,
   }
 
+  if $::fluentd::install_catch_all {
+    package { 'google-fluentd-catch-all-config':
+      ensure => installed,
+    }
+  }
 }
